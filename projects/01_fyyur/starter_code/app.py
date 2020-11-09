26e3e231d9b34f1a8b541de68e580de189f42ea1
tomvonheill <tomvonheill@gmail.com>
2020-11-09 17:12:43 -0600

    artist seach implemented

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -307,14 +307,18 @@ def search_artists():
   # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
   # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
   # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
+  serach_term = request.form.get('search_term', '')
+  artists = db.session.query(Artist.id, Artist.name).filter(func.lower(Artist.name).contains(func.lower(serach_term)))
+  response = {'count':0,
+  'data': []}
+
+  for artist in artists:
+    sub_data = {
+      'id':artist.id,
+      'name':artist.name
+    }
+    response['count'] = response['count']+1
+    response['data'].append(sub_data)
   return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
